43860b059050ac700a0ac19cf4571609fc91f3f8
Unknown <brenekharrison@gmail.com>
2018-07-21 11:55:05 -0600

    Basically just fixing the merge nightmare.

---
+++ menus.py
@@ -0,0 +1,15 @@
+import pygame
+
+class MenuPanel(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.__init__(self)
+
+    def addButton(self, buttonObj):
+        return
+    
+    def addText(self, textObj):
+        return
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.__init__(self)
